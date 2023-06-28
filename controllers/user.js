6964b8ab14ac769f7b8a990c95bae25900b7c570
krzysztofKrowka <krzysztof.krowka@onet.eu>
2023-06-28 13:14:40 +0200

    add gitignore env

---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+exports.postRegisterUser = (req, res, next) => {};
+
+exports.getRegisterUser = (req, res, next) => {};
+
+exports.postLoginUser = (req, res, next) => {};
+
+exports.getLoginUser = (req, res, next) => {};
